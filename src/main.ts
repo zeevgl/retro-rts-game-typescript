4f4ea1a34eee93fdcd83d7e4dd1384c461bff440
zeevg <zeevg@wix.com>
2023-01-11 13:54:01 +0200

    move index.js to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+function initHTML() {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <canvas
       id="gameScreen"
       width="1"
@@ -10,5 +11,40 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       Your browser does not support the HTML5 canvas tag.
     </canvas>
 `;
+}
 
-window.initGame();
+function initGame() {
+  const GAME_WIDTH = window.innerWidth;
+  const GAME_HEIGHT = window.innerHeight;
+
+  initHTML();
+
+  const canvas: HTMLCanvasElement = document.getElementById("gameScreen") as HTMLCanvasElement;
+  canvas.setAttribute("width", GAME_WIDTH.toString());
+  canvas.setAttribute("height", GAME_HEIGHT.toString());
+  const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+  ctx.imageSmoothingEnabled = false;
+  const game = new Game(GAME_WIDTH, GAME_HEIGHT, canvas);
+
+  let lastTime = 0;
+
+  function gameLoop(timestamp: number) {
+    /*
+      deltaTime - milliseconds since last frame
+      deltaTime / 1000 - seconds since last frame
+     */
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+    game.update(deltaTime, timestamp);
+    game.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+  }
+
+  requestAnimationFrame(gameLoop);
+}
+
+initGame();
