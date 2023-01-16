bb0d8dbf6de8ee467df1b5036cd37a753a800039
zeevg <zeevg@wix.com>
2023-01-16 16:12:26 +0200

    disable  js file

--- public/js/players/resources.js
+++ public/js/players/resources.js
@@ -1,45 +1,45 @@
-window["Resources"] = (() => {
-  class Resources {
-    constructor(player) {
-      this.player = player;
-      this.money = 0;
-      this.increaseAmount = 0;
-      this.rate = 1;
-      this.increaseBy = 12;
-      this.tick = 0;
-    }
-
-    update(deltaTime, timestamp) {
-      if (this.increaseAmount !== 0) {
-        this.tick += deltaTime;
-        if (this.tick >= this.rate) {
-          this.tick = 0;
-
-          const sign = Math.abs(this.increaseAmount) / this.increaseAmount;
-          const abs = Math.abs(this.increaseAmount - sign * this.increaseBy);
-          if (abs >= this.increaseBy) {
-            this.money += sign * this.increaseBy;
-            this.increaseAmount -= sign * this.increaseBy;
-          } else {
-            this.money += this.increaseAmount;
-            this.increaseAmount = 0;
-          }
-        }
-      }
-    }
-
-    addResources(amount) {
-      this.increaseAmount += Math.round(amount);
-    }
-
-    deductResources(amount) {
-      this.increaseAmount -= Math.round(amount);
-    }
-
-    canAfford(cost) {
-      return this.money >= cost;
-    }
-  }
-
-  return Resources;
-})();
+// window["Resources"] = (() => {
+//   class Resources {
+//     constructor(player) {
+//       this.player = player;
+//       this.money = 0;
+//       this.increaseAmount = 0;
+//       this.rate = 1;
+//       this.increaseBy = 12;
+//       this.tick = 0;
+//     }
+//
+//     update(deltaTime, timestamp) {
+//       if (this.increaseAmount !== 0) {
+//         this.tick += deltaTime;
+//         if (this.tick >= this.rate) {
+//           this.tick = 0;
+//
+//           const sign = Math.abs(this.increaseAmount) / this.increaseAmount;
+//           const abs = Math.abs(this.increaseAmount - sign * this.increaseBy);
+//           if (abs >= this.increaseBy) {
+//             this.money += sign * this.increaseBy;
+//             this.increaseAmount -= sign * this.increaseBy;
+//           } else {
+//             this.money += this.increaseAmount;
+//             this.increaseAmount = 0;
+//           }
+//         }
+//       }
+//     }
+//
+//     addResources(amount) {
+//       this.increaseAmount += Math.round(amount);
+//     }
+//
+//     deductResources(amount) {
+//       this.increaseAmount -= Math.round(amount);
+//     }
+//
+//     canAfford(cost) {
+//       return this.money >= cost;
+//     }
+//   }
+//
+//   return Resources;
+// })();
--- public/js/units/techTree.js
+++ public/js/units/techTree.js
@@ -1,91 +1,91 @@
-window["TechTree"] = (() => {
-  class TechTree {
-    constructor(player) {
-      this.player = player;
-      this.init();
-    }
-
-    init() {
-      this.buildings = [
-        {
-          unit: new ContractionYard({ x: 0, y: 0, color: "gray" }),
-          class: ContractionYard,
-          isVisible: false,
-          isUnlocked: () => false,
-          exists: true,
-        },
-        {
-          unit: new Refinery({ x: 0, y: 0, color: "gray" }),
-          class: Refinery,
-          isVisible: true,
-          isUnlocked: () => this.checkDependencies([ContractionYard]),
-          exists: false,
-        },
-        {
-          unit: new Barracks({ x: 0, y: 0, color: "gray" }),
-          class: Barracks,
-          isVisible: true,
-          isUnlocked: () => this.checkDependencies([ContractionYard]),
-          exists: false,
-        },
-      ];
-
-      this.units = [
-        {
-          unit: new Infantry({ x: 0, y: 0, color: "gray" }),
-          class: Infantry,
-          isVisible: true,
-          isUnlocked: () => this.checkDependencies([Barracks]),
-        },
-        {
-          unit: new Rocketeer({ x: 0, y: 0, color: "gray" }),
-          class: Rocketeer,
-          isVisible: true,
-          isUnlocked: () => this.checkDependencies([Barracks]),
-        },
-        {
-          unit: new Harvester({ x: 0, y: 0, color: "gray" }),
-          class: Harvester,
-          isVisible: true,
-          isUnlocked: () => this.checkDependencies([Refinery]),
-        },
-      ];
-    }
-
-    checkDependencies(dependencies) {
-      return dependencies.every((dependency) => {
-        return this.buildings.some((building) => {
-          return building.unit instanceof dependency && building.exists;
-        });
-      });
-    }
-
-    getVisibleUnits() {
-      return this.units.filter((unit) => unit.isVisible);
-    }
-
-    getVisibleFightingUnits() {
-      return this.units.filter((unit) => unit.isVisible && unit.unit.group === UnitGroups.fighter);
-    }
-
-    getVisibleBuildings() {
-      return this.buildings.filter((building) => building.isVisible);
-    }
-
-    getHarvester() {
-      return this.units.find((unit) => unit.unit instanceof Harvester);
-    }
-
-    updateTechTree(unit) {
-      if (unit.isABuilding()) {
-        this.buildings.forEach((building) => {
-          if (unit instanceof building.class) {
-            building.exists = true;
-          }
-        });
-      }
-    }
-  }
-
-  return TechTree;
-})();
+// window["TechTree"] = (() => {
+//   class TechTree {
+//     constructor(player) {
+//       this.player = player;
+//       this.init();
+//     }
+//
+//     init() {
+//       this.buildings = [
+//         {
+//           unit: new ContractionYard({ x: 0, y: 0, color: "gray" }),
+//           class: ContractionYard,
+//           isVisible: false,
+//           isUnlocked: () => false,
+//           exists: true,
+//         },
+//         {
+//           unit: new Refinery({ x: 0, y: 0, color: "gray" }),
+//           class: Refinery,
+//           isVisible: true,
+//           isUnlocked: () => this.checkDependencies([ContractionYard]),
+//           exists: false,
+//         },
+//         {
+//           unit: new Barracks({ x: 0, y: 0, color: "gray" }),
+//           class: Barracks,
+//           isVisible: true,
+//           isUnlocked: () => this.checkDependencies([ContractionYard]),
+//           exists: false,
+//         },
+//       ];
+//
+//       this.units = [
+//         {
+//           unit: new Infantry({ x: 0, y: 0, color: "gray" }),
+//           class: Infantry,
+//           isVisible: true,
+//           isUnlocked: () => this.checkDependencies([Barracks]),
+//         },
+//         {
+//           unit: new Rocketeer({ x: 0, y: 0, color: "gray" }),
+//           class: Rocketeer,
+//           isVisible: true,
+//           isUnlocked: () => this.checkDependencies([Barracks]),
+//         },
+//         {
+//           unit: new Harvester({ x: 0, y: 0, color: "gray" }),
+//           class: Harvester,
+//           isVisible: true,
+//           isUnlocked: () => this.checkDependencies([Refinery]),
+//         },
+//       ];
+//     }
+//
+//     checkDependencies(dependencies) {
+//       return dependencies.every((dependency) => {
+//         return this.buildings.some((building) => {
+//           return building.unit instanceof dependency && building.exists;
+//         });
+//       });
+//     }
+//
+//     getVisibleUnits() {
+//       return this.units.filter((unit) => unit.isVisible);
+//     }
+//
+//     getVisibleFightingUnits() {
+//       return this.units.filter((unit) => unit.isVisible && unit.unit.group === UnitGroups.fighter);
+//     }
+//
+//     getVisibleBuildings() {
+//       return this.buildings.filter((building) => building.isVisible);
+//     }
+//
+//     getHarvester() {
+//       return this.units.find((unit) => unit.unit instanceof Harvester);
+//     }
+//
+//     updateTechTree(unit) {
+//       if (unit.isABuilding()) {
+//         this.buildings.forEach((building) => {
+//           if (unit instanceof building.class) {
+//             building.exists = true;
+//           }
+//         });
+//       }
+//     }
+//   }
+//
+//   return TechTree;
+// })();
