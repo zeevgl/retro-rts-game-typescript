8d205def0600708cadf3c13917f8e3dac5a813f1
zeevg <zeevg@wix.com>
2023-01-16 17:23:40 +0200

    disable  js file

--- public/js/utils/frameAnimator.js
+++ public/js/utils/frameAnimator.js
@@ -1,78 +1,78 @@
-class FrameAnimator {
-  constructor(
-    sprite,
-    startIndex,
-    endIndex,
-    loop = false,
-    frameDuration = 80,
-    { onComplete = null } = {}
-  ) {
-    this.sprite = sprite;
-    this.startIndex = startIndex;
-    this.endIndex = endIndex;
-    this.loop = loop;
-    this.frameDuration = frameDuration; //ms
-    this.onComplete = onComplete;
-
-    this.imageFrame = null;
-    this.isRunning = false;
-    this.totalDt = 0;
-  }
-
-  static fromAnimationFrame(
-    sprite,
-    { start, length, loop, frameDuration },
-    options = undefined
-  ) {
-    return new this(
-      sprite,
-      start,
-      start + length,
-      loop,
-      frameDuration,
-      options
-    );
-  }
-
-  update(dt, timestamp) {
-    if (!this.isRunning) {
-      return;
-    }
-
-    this.totalDt += dt;
-
-    if (this.totalDt >= this.frameDuration) {
-      if (this.imageFrame >= this.endIndex) {
-        if (this.loop) {
-          this.imageFrame = this.startIndex;
-        } else {
-          this.stop();
-          this.onComplete?.();
-        }
-      } else {
-        this.imageFrame++;
-      }
-
-      this.totalDt = 0;
-    }
-  }
-
-  draw(canvas, x, y) {
-    //use draw to render OR use getActiveFrame manually render
-    this.sprite.draw(canvas, this.imageFrame, x, y);
-  }
-
-  getActiveFrame() {
-    return this.imageFrame;
-  }
-
-  start() {
-    this.imageFrame = this.startIndex;
-    this.totalDt = 0;
-    this.isRunning = true;
-  }
-
-  stop() {
-    this.isRunning = false;
-  }
-}
+// class FrameAnimator {
+//   constructor(
+//     sprite,
+//     startIndex,
+//     endIndex,
+//     loop = false,
+//     frameDuration = 80,
+//     { onComplete = null } = {}
+//   ) {
+//     this.sprite = sprite;
+//     this.startIndex = startIndex;
+//     this.endIndex = endIndex;
+//     this.loop = loop;
+//     this.frameDuration = frameDuration; //ms
+//     this.onComplete = onComplete;
+//
+//     this.imageFrame = null;
+//     this.isRunning = false;
+//     this.totalDt = 0;
+//   }
+//
+//   static fromAnimationFrame(
+//     sprite,
+//     { start, length, loop, frameDuration },
+//     options = undefined
+//   ) {
+//     return new this(
+//       sprite,
+//       start,
+//       start + length,
+//       loop,
+//       frameDuration,
+//       options
+//     );
+//   }
+//
+//   update(dt, timestamp) {
+//     if (!this.isRunning) {
+//       return;
+//     }
+//
+//     this.totalDt += dt;
+//
+//     if (this.totalDt >= this.frameDuration) {
+//       if (this.imageFrame >= this.endIndex) {
+//         if (this.loop) {
+//           this.imageFrame = this.startIndex;
+//         } else {
+//           this.stop();
+//           this.onComplete?.();
+//         }
+//       } else {
+//         this.imageFrame++;
+//       }
+//
+//       this.totalDt = 0;
+//     }
+//   }
+//
+//   draw(canvas, x, y) {
+//     //use draw to render OR use getActiveFrame manually render
+//     this.sprite.draw(canvas, this.imageFrame, x, y);
+//   }
+//
+//   getActiveFrame() {
+//     return this.imageFrame;
+//   }
+//
+//   start() {
+//     this.imageFrame = this.startIndex;
+//     this.totalDt = 0;
+//     this.isRunning = true;
+//   }
+//
+//   stop() {
+//     this.isRunning = false;
+//   }
+// }
--- public/js/utils/pointsCalc.js
+++ public/js/utils/pointsCalc.js
@@ -1,61 +1,61 @@
-function calcDistance(x1, y1, x2, y2) {
-  const dx = x2 - x1;
-  const dy = y2 - y1;
-  return Math.sqrt(dx * dx + dy * dy);
-}
-
-function calcMoves(speed, distance, x1, y1, x2, y2) {
-  const moves = distance / speed;
-  const xunits = (x2 - x1) / moves;
-  const yunits = (y2 - y1) / moves;
-
-  return {
-    xunits,
-    yunits,
-  };
-}
-
-function getClosestUnitOfPlayer(
-  fromUnit,
-  player,
-  { unitTypeClass = null, ignoreVisionRange = true } = {}
-) {
-  return player.units
-    .filter((unit) => {
-      if (unitTypeClass) {
-        return unit.isAlive && unit instanceof unitTypeClass;
-      }
-      return unit.isAlive;
-    })
-    .map((unit) => ({
-      unit,
-      distance: calcDistance(fromUnit.x, fromUnit.y, unit.x, unit.y),
-    }))
-    .filter(
-      (unit) => ignoreVisionRange || unit.distance <= fromUnit.visionRange
-    )
-    .reduce(
-      (prev, curr) => (prev?.distance < curr?.distance ? prev : curr),
-      null
-    );
-}
-
-function getDegree360(x1, y1, x2, y2) {
-  const radian = Math.atan2(y2 - y1, x2 - x1);
-  return radian * (180 / Math.PI) + 180;
-}
-
-function getDegree180(x1, y1, x2, y2) {
-  const radians = Math.atan2(y2 - y1, x2 - x1);
-  const degrees = radians * (180 / Math.PI);
-  return degrees;
-}
-
-function getRadian(x1, y1, x2, y2) {
-  //currently not used
-  const radian = Math.atan2(y2 - y1, x2 - x1);
-  const degrees = Math.atan(y2 - y1, x2 - x1);
-  //console.log('radian = ', radian, radian * (180 / Math.PI));
-  return radian + 2 * Math.PI;
-  //http://jsfiddle.net/rjCeV/2/
-}
+// function calcDistance(x1, y1, x2, y2) {
+//   const dx = x2 - x1;
+//   const dy = y2 - y1;
+//   return Math.sqrt(dx * dx + dy * dy);
+// }
+//
+// function calcMoves(speed, distance, x1, y1, x2, y2) {
+//   const moves = distance / speed;
+//   const xunits = (x2 - x1) / moves;
+//   const yunits = (y2 - y1) / moves;
+//
+//   return {
+//     xunits,
+//     yunits,
+//   };
+// }
+//
+// function getClosestUnitOfPlayer(
+//   fromUnit,
+//   player,
+//   { unitTypeClass = null, ignoreVisionRange = true } = {}
+// ) {
+//   return player.units
+//     .filter((unit) => {
+//       if (unitTypeClass) {
+//         return unit.isAlive && unit instanceof unitTypeClass;
+//       }
+//       return unit.isAlive;
+//     })
+//     .map((unit) => ({
+//       unit,
+//       distance: calcDistance(fromUnit.x, fromUnit.y, unit.x, unit.y),
+//     }))
+//     .filter(
+//       (unit) => ignoreVisionRange || unit.distance <= fromUnit.visionRange
+//     )
+//     .reduce(
+//       (prev, curr) => (prev?.distance < curr?.distance ? prev : curr),
+//       null
+//     );
+// }
+//
+// function getDegree360(x1, y1, x2, y2) {
+//   const radian = Math.atan2(y2 - y1, x2 - x1);
+//   return radian * (180 / Math.PI) + 180;
+// }
+//
+// function getDegree180(x1, y1, x2, y2) {
+//   const radians = Math.atan2(y2 - y1, x2 - x1);
+//   const degrees = radians * (180 / Math.PI);
+//   return degrees;
+// }
+//
+// function getRadian(x1, y1, x2, y2) {
+//   //currently not used
+//   const radian = Math.atan2(y2 - y1, x2 - x1);
+//   const degrees = Math.atan(y2 - y1, x2 - x1);
+//   //console.log('radian = ', radian, radian * (180 / Math.PI));
+//   return radian + 2 * Math.PI;
+//   //http://jsfiddle.net/rjCeV/2/
+// }
